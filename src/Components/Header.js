904a02529abffda2cdf39850ab426c584420438e
BrentonPedler <brenton_pedler@hotmail.com>
2022-01-27 17:35:39 -0500

    added img from api call

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header>
             <h1>SPACE-X: Rocket Hub 2.0</h1>
-            <h2>Ground control to Major Tom</h2>
+            {/* <h2>Ground control to Major Tom</h2> */}
             <div className="info">
                 <p>Learn all there is to know about Space X rockets! All data is generated from the Space X API.</p>
             </div>
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -3,14 +3,18 @@ import React from 'react';
 const Results = ({data}) => {
 
     let { description, name } = data;
-
-    console.log(data);
+    let imageOne = data.flickr_images[0];
+    let imageTwo = data.flickr_images[1];
     
     
     return (
         <div className="results">
             <h3>{name}</h3>
             <p>{description}</p>
+            <div className="images">
+                <img src={imageOne} alt="" />
+                <img src={imageTwo} alt="" />
+            </div>
         </div>
 
     )
